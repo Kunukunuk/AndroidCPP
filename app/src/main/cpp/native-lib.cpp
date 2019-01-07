7ed11f0e25bb23f75a84b1637eba9b6c597cf785
Kun Huang <kkun.huang@gmail.com>
2019-01-06 20:04:38 -0500

    added button and list view

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,12 @@
 #include <jni.h>
 #include <string>
 
+using namespace std;
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_kunhuang_newstring_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
--- app/src/main/java/com/example/kunhuang/newstring/MainActivity.java
+++ app/src/main/java/com/example/kunhuang/newstring/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.kunhuang.newstring;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+        Button getStringButton = (Button) findViewById(R.id.getsringbutton);
+        getStringButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 
     /**
